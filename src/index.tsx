771af64b28f29be90cee6e860b8df6f1e3c777f3
Garrett Weems <gwgraphicdesign@gmail.com>
2019-08-27 21:50:53 -0500

    :art: added className / id prop

--- src/index.tsx
+++ src/index.tsx
@@ -9,29 +9,6 @@ import {
   WidthProperty
 } from 'csstype';
 
-/* Embed Options
-
-The options shown in the embed modal are not all options available. We need a
-new UI for the share model to reflect these options, in the meantime you can
-find them here.
-
-Option           Description / Values     -  Default
-
-autoresize      -  0/1                    -  0      -  Automatically resize the embed to the content (only works on Medium).
-codemirror      -  0/1                    -  0      -  Use CodeMirror editor instead of Monaco (decreases embed size significantly).
-editorsize      -  number                 -  50     -  Size in percentage of editor.
-eslint          -  0/1                    -  0      -  Use eslint (increases embed size significantly).
-expanddevtools  -  0/1                    -  0      -  Start with the devtools (console) open.
-fontsize        -  number (in px)         -  14     -  Font size of editor
-forcerefresh    -  0/1                    -  0      -  Force a full refresh of frame after every edit.
-hidenavigation  -  0/1                    -  0      -  Hide the navigation bar of the preview.
-highlights      -  [num, num, num]        -  null   -  Which lines to highlight (only works in CodeMirror)
-initialpath     --  string                -  /      -  Which url to initially load in address bar
-module          --  opened files          -  null
-                    Multiple paths comma separated are allowed,
-                    in that case we show them as tabs | path to module (starting with `/`) */
-// type ZeroOrOne = number;
-
 interface SandboxQuery<Type> {
   codeMirror?: Type;
   editorSize?: number;
@@ -51,10 +28,11 @@ interface SandboxQuery<Type> {
 }
 
 interface CodeSandbox extends SandboxQuery<boolean> {
+  id?: string;
+  className?: string;
   sandbox?: string;
   git?: [string, string, string, string?];
   title?: string;
-  className?: string;
   style?: StyleSheetList;
   width?: WidthProperty<string>;
   height?: HeightProperty<string>;
@@ -66,6 +44,8 @@ interface CodeSandbox extends SandboxQuery<boolean> {
 const boolToNum = (bool: boolean): 1 | 0 => (bool ? 1 : 0);
 
 const CodeSandbox = ({
+  id,
+  className = 'codesandbox',
   sandbox = 'github/glweems/empty-sandbox/tree/dev/',
   git,
   title = 'code sandbox embed',
@@ -84,10 +64,9 @@ const CodeSandbox = ({
   runOnClick = false,
   verticalLayout = false,
   view = 'split',
-  className,
   style,
   width = '100%',
-  height = '50vh',
+  height = '500px',
   border = '0',
   borderRadius = '4px'
 }: CodeSandbox): JSX.Element => {
@@ -123,9 +102,10 @@ const CodeSandbox = ({
 
   return (
     <iframe
-      src={url()}
-      title={title}
+      id={id}
       className={className}
+      title={title}
+      src={url()}
       allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media"
       sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
       style={{
